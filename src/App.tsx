1d15fffc4f005d231abb3d430b98ceb16a12482f
CarlosLichowski <carlosjlicho@gmail.com>
2025-02-05 18:02:14 -0300

    testing new cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import NavBar from './components/Navbar/NavBar';
 import './App.css';
 import { OurOffers } from './components/OurOffers/OurOffers';
 import SobreNosotros from './components/AboutUs/AboutUs';
-import Servicios from './components/Servicios/Servicios';
+import Servicios from './components/ServiciosTest copy/Servicios';
 import Banner from './components/Banner/Banner';
 import ConsultaForm from './components/Contactanos/ConsultaForm';
 
